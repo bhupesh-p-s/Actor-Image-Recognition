a26a287edf99c1b9f64d98e3ad6ba327095bec37
Bhupesh P S <77381824+bhupesh-p-s@users.noreply.github.com>
2023-01-15 20:36:11 +0530

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+
+@app.route("/classify_image", methods=['GET','POST'])
+def classify_image():
+    image_data = request.form['image_data']
+    response=jsonify(util.classify_image(image_data))
+    response.headers.add('Access-control-Allow-Origin','*')
+    return response
+
+if __name__ == "__main__":
+    print("Starting Tamil Actors Classifier")
+    util.load_saved_artifacts()
+    app.run(port=5000)
